623b09c57578a6effaee4e42078d9f4428283fef
Vinay Kumar <vinaykumar.borra01@gmail.com>
2023-07-23 13:24:03 +0530

    Updated and secure
    Added ReentrancyGuard: The contract now includes protection against reentrancy attacks to ensure the safety of contract functions.
    
    Slashing Percentage and Time Chain Periods: The contract now calculates and reserves the slashing amount for new supplies of INT based on the slashing percentage and remaining time chain periods.
    
    updateSlashingPercentage and updateTimeChainPeriods: New functions have been added to allow the contract owner to update the slashing percentage and time chain periods, providing more flexibility in managing the contract parameters.

--- MintingPool.sol
+++ MintingPool.sol
@@ -3,15 +3,18 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./INT.sol";
 
-contract MintingPool is Ownable {
+contract MintingPool is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
     INT public intToken;
     address public verifier;
     uint256 public developerRewardPercentage = 90;
     uint256 public verifierRewardPercentage = 10;
+    uint256 public slashingPercentage = 2 * 10 ** 6; // 2% in 6 decimal places
+    uint256 public timeChainPeriods = 50;
 
     event Minted(address indexed account, uint256 amount);
 
@@ -20,8 +23,12 @@ contract MintingPool is Ownable {
         verifier = _verifier;
     }
 
-    function mintByIssueSolvers(uint256 amount) external {
+    function mintByIssueSolvers(uint256 amount) external nonReentrant {
         require(amount > 0, "Invalid amount");
+        require(
+            intToken.balanceOf(msg.sender) >= amount,
+            "Insufficient balance"
+        );
 
         uint256 totalSupply = intToken.totalSupply();
         uint256 verifierReward = amount.mul(verifierRewardPercentage).div(100);
@@ -32,5 +39,31 @@ contract MintingPool is Ownable {
 
         emit Minted(msg.sender, developerReward);
         emit Minted(verifier, verifierReward);
+
+        // Calculate the slashing amount for the new supply of INT
+        uint256 newSupply = totalSupply.add(amount);
+        uint256 remainingTimeChainPeriods = timeChainPeriods.sub(
+            block.number % timeChainPeriods
+        );
+        uint256 slashingAmount = slashingPercentage.mul(amount).div(
+            remainingTimeChainPeriods
+        );
+
+        // Reserve the slashing amount in the GLIPs vault
+        intToken.mint(address(this), slashingAmount);
+    }
+
+    function updateSlashingPercentage(
+        uint256 _newSlashingPercentage
+    ) external onlyOwner {
+        require(_newSlashingPercentage > 0, "Invalid slashing percentage");
+        slashingPercentage = _newSlashingPercentage;
+    }
+
+    function updateTimeChainPeriods(
+        uint256 _newTimeChainPeriods
+    ) external onlyOwner {
+        require(_newTimeChainPeriods > 0, "Invalid time chain periods");
+        timeChainPeriods = _newTimeChainPeriods;
     }
 }
